2d692942d824b95a64eeb15b303b3a40bb44e39c
Andrey Tunyatkin <andtun@yandex.ru>
2019-03-12 14:59:22 +0300

    mem errors 2nd try

--- main.cpp
+++ main.cpp
@@ -1,52 +1,30 @@
-#include <chrono>
 #include <iostream>
+
 using namespace std;
-
-class Timer
-{
-    using clock_t = std::chrono::high_resolution_clock;
-    using microseconds = std::chrono::microseconds;
-public:
-    Timer()
-        : start_(clock_t::now())
-    {
-    }
-
-    ~Timer()
-    {
-        const auto finish = clock_t::now();
-        const auto us = 
-            std::chrono::duration_cast<microseconds>
-                (finish - start_).count();
-        std::cout << us << " us" << std::endl;
-    }
-
-private:
-    const clock_t::time_point start_;
-};
+using array = int*;
+using matrix = int(*)[2];
+using int64 = uint64_t;
 
 int main() {
-    const uint64_t n_rows = 100000000;
-    const uint64_t n_cols = 10;
-
-    using array = int*;
-    using matrix = int(*)[2];
-    array T = new int[n_cols];
-    matrix A = (matrix) new int[n_rows][n_cols];
-
-    for (uint64_t i = 0; i < n_cols; i++) {
-        T[i] = i;
+    /*
+    const int64 size = 1000000000;
+    int* A = new int[size];
+    for (int64 i = 0; i < size; i++) {
+        A[i] = i;
     };
-    cout << T[45687] << endl;
+    cout << A[889135] << endl; */
 
-    for (uint64_t i = 0; i < n_cols; i++) {
-        for (uint64_t j = 0; j < n_rows; j++) {
-            A[j][i] = i + j;
+    const int64 height = 10000;
+    const int64 width = 10000;
+
+    matrix T = (matrix) new int[width][height];
+
+    for (int64 i = 0; i < height; i++) {
+        for (int64 j = 0; j < width; j++) {
+            T[j][i] = i + j;
         };
-        //cout << A[0][i] << endl;
     };
-    cout << A[100][4];
+    cout << T[8][8] << endl;
 
     return 0;
 }
-
