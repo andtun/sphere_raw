ea9ef25c4c9dd5b826459d1f57d439284dfb01f0
Andrey Tunyatkin <andtun@yandex.ru>
2019-03-13 17:56:10 +0300

    memory errors

--- main.cpp
+++ main.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
-
 using namespace std;
-using array = int*;
-using matrix = int(*)[2];
 using int64 = uint64_t;
 
 int main() {
-    /*
-    const int64 size = 1000000000;
-    int* A = new int[size];
-    for (int64 i = 0; i < size; i++) {
-        A[i] = i;
+    const int64 n_rows = 10000000000;
+    const int64 n_cols = 10000000000;
+
+    int** ptr = new int*[n_rows];
+
+    for(int64 i = 0; i < n_rows; i++) {
+        ptr[i]=new int[n_cols];
     };
-    cout << A[889135] << endl; */
 
-    const int64 height = 10000;
-    const int64 width = 10000;
-
-    matrix T = (matrix) new int[width][height];
-
-    for (int64 i = 0; i < height; i++) {
-        for (int64 j = 0; j < width; j++) {
-            T[j][i] = i + j;
-        };
+    ptr[7][7] = 154;
+    cout << ptr[7][7];
+    cout << ptr[3][4];
+     
+    for(int64 i = 0; i < n_rows; i++) {
+        delete ptr[i];
     };
-    cout << T[8][8] << endl;
 
+    delete[] ptr;
+   
     return 0;
 }
